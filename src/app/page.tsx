0956075d1642d713312d34fc22b18ebee6354f7e
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-19 00:18:27 +0530

    update Home component to include background image with improved path resolution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,21 @@
 import Image from "next/image";
-import homeImg from "@/public/home.jpg";
+
+import homeImg from "../../public/home.jpg";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+          src={homeImg}
+          alt="Home"
+        />
+      </div>
+    </div>
+  );
 }
